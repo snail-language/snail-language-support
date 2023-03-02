12a7d9f93493b2c7dd5f5d6d18e31dd4b4690f19
Kevin Angstadt <kangstadt@stlawu.edu>
2023-03-02 10:03:36 -0500

    Updates to debug adapter

--- client/src/debugAdapter.ts
+++ client/src/debugAdapter.ts
@@ -3,24 +3,33 @@
 // is running separately in node
 
 import * as s from 'net';
+import * as f from 'fs';
+
+// FIXME debugging
+const file = '/Users/kangstadt/git/snail-language-support/stderr.txt'
+const file2 = '/Users/kangstadt/git/snail-language-support/stdin.txt'
+f.writeFileSync(file, 'Debug Output\n');
+f.writeFileSync(file2, 'Debug Input\n');
 
 
 const PORT_NUM = 9999;
 var client = s.connect(PORT_NUM, 'localhost', () => {
-    console.error("debugAdapter connected")
+    f.appendFileSync(file, "debugAdapter connected\n")
 });
 
 client.on('data', (buff) => {
     const content : String = buff.toString('utf-8');
-    console.log(content, " from tcp\n");
+    console.log(content);
+    f.appendFileSync(file, content.toString());
 })
 
 client.on('error', (err) => {
-    console.log("Error!");
-    console.log(err);
+    f.writeFileSync(file, "Error!\n");
+    f.writeFileSync(file, err.toString());
 })
 
 process.stdin.on('data', (buff) => {
     const content : String = buff.toString('utf-8');
     client.write(content + " from VSCode\n")
+    f.writeFileSync(file2, content.toString());
 })
